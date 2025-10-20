febccf45d73c0f543703288843ecdf69368fc9e2
Tohru Yaginuma <toru45s@gmail.com>
2025-10-20 16:03:12 -0700

    add user repo

---
+++ src/repository/user.ts
@@ -0,0 +1,15 @@
+import type { User, UserId } from "@/domain/user";
+import type { IUserRepository } from "@/domain/user-repository";
+
+export class UserRepository implements IUserRepository {
+  readonly #users: ReadonlyArray<User>;
+
+  constructor(users: ReadonlyArray<User>) {
+    this.#users = users;
+  }
+
+  async findById(id: UserId): Promise<User | undefined> {
+    console.log("user repository", this.#users);
+    return this.#users.find((user) => user.id === id);
+  }
+}
