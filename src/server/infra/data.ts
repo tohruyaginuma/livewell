41841bb94a8e362bb012fed02716889b23fdb98e
Tohru Yaginuma <toru45s@gmail.com>
2025-10-21 22:51:37 -0700

    update data

--- src/server/infra/data.ts
+++ src/server/infra/data.ts
@@ -1,33 +1,11 @@
 import dayjs from "dayjs";
 
-export const users = [
-  {
-    id: 1,
-    name: "John Doe 1",
-  },
-  {
-    id: 2,
-    name: "John Doe 2",
-  },
-  {
-    id: 3,
-    name: "John Doe 3",
-  },
-];
+export const users = [{ id: 1, name: "John Doe 1" }];
 
 export const medications = [
-  {
-    id: 1,
-    name: "Medication 1",
-  },
-  {
-    id: 2,
-    name: "Medication 2",
-  },
-  {
-    id: 3,
-    name: "Medication 3",
-  },
+  { id: 1, name: "Medication 1" },
+  { id: 2, name: "Medication 2" },
+  { id: 3, name: "Medication 3" },
 ];
 
 export const userMedications = [
@@ -36,52 +14,57 @@ export const userMedications = [
     userId: users[0].id,
     medicationId: medications[0].id,
     quantityReceived: 300,
-    startDate: dayjs().subtract(10, "day").toDate(),
     dosage: 2,
+    frequency: 2,
+    daysSupply: 30,
+    startDate: dayjs().subtract(10, "day").format("YYYY-MM-DD"),
   },
   {
     id: 2,
     userId: users[0].id,
     medicationId: medications[1].id,
-    quantityReceived: 30,
-    startDate: dayjs().subtract(2, "day").toDate(),
-    dosage: 2,
+    quantityReceived: 60,
+    dosage: 1,
+    frequency: 1,
+    daysSupply: 60,
+    startDate: dayjs().subtract(5, "day").format("YYYY-MM-DD"),
   },
   {
     id: 3,
     userId: users[0].id,
-    medicationId: medications[1].id,
-    quantityReceived: 30,
-    startDate: dayjs().subtract(10, "day").toDate(),
-    dosage: 2,
-  },
-  {
-    id: 4,
-    userId: users[0].id,
-    medicationId: medications[1].id,
-    quantityReceived: 30,
-    startDate: dayjs().subtract(10, "day").toDate(),
-    dosage: 10,
+    medicationId: medications[2].id,
+    quantityReceived: 90,
+    dosage: 3,
+    frequency: 3,
+    daysSupply: 30,
+    startDate: dayjs().subtract(20, "day").format("YYYY-MM-DD"),
   },
 ];
 
-export const userMedicationStatus = [
+export const userMedicationStatuses = [
   {
     id: 1,
-    userId: users[0].id,
     userMedicationId: userMedications[0].id,
-    takenDate: dayjs().subtract(10, "day").toDate(),
+    takenDate: dayjs().subtract(10, "day").format("YYYY-MM-DD"),
   },
   {
     id: 2,
-    userId: users[0].id,
     userMedicationId: userMedications[0].id,
-    takenDate: dayjs().subtract(9, "day").toDate(),
+    takenDate: dayjs().subtract(9, "day").format("YYYY-MM-DD"),
   },
   {
     id: 3,
-    userId: users[0].id,
     userMedicationId: userMedications[0].id,
-    takenDate: dayjs().subtract(8, "day").toDate(),
+    takenDate: dayjs().subtract(8, "day").format("YYYY-MM-DD"),
+  },
+  {
+    id: 4,
+    userMedicationId: userMedications[1].id,
+    takenDate: dayjs().subtract(5, "day").format("YYYY-MM-DD"),
+  },
+  {
+    id: 5,
+    userMedicationId: userMedications[1].id,
+    takenDate: dayjs().subtract(4, "day").format("YYYY-MM-DD"),
   },
 ];
